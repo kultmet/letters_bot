35aa1248e1e20a5c7363f5b678034eb8fda88cb5
Maystrenko Dmitriy <kultmet@gmail.com>
2023-05-12 13:07:10 +0300

    redacted hostname

--- constants.py
+++ constants.py
@@ -4,7 +4,7 @@ COMMANDS_OFFER = """
 /letter - команда для создания сопроводительного письма;
 /my_skills - посмотреть все скиллы пользователя;
 /add_my_skill добавить скилл в ваш стэк."""
-API_HOST = 'http://127.0.0.1:8000'
+API_HOST = 'http://127.0.0.1'
 API_PORT = '8000'
 
 # endpoint for adding user's skill
